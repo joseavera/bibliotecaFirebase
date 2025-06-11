b30face6bcdfbba5327db85f2043e772e5308dd4
@joseavera <69088516+joseavera@users.noreply.github.com>
2025-06-11 11:25:19 -0600

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,14 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
 
- const firebaseConfig = {
-    apiKey: "AIzaSyBTU",
+  const firebaseConfig = {
+    apiKey: "AIzaSyBZEVISZuRfn1iC3be5b7xTMQiCZubXyTU",
     authDomain: "bdnosql-de619.firebaseapp.com",
-    projectId: "b19",
-    storageBucket: "bdnoapp",
-    messagingSenderId: "519",
-    appId: "1:5193611",
-    measurementId: "G0"
+    projectId: "bdnosql-de619",
+    storageBucket: "bdnosql-de619.firebasestorage.app",
+    messagingSenderId: "519369652548",
+    appId: "1:519369652548:web:260c2b45275f5a0ff5ff11",
+    measurementId: "G-1FYEJLG220"
   };
 
 const app = initializeApp(firebaseConfig);
